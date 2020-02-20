0d4b6ee95ad99c9c8ec56456667bc8261db60afd
phuc <Ngoc Phuc@DESKTOP-VPNO4HN>
2020-02-20 17:09:11 +0700

    up

---
+++ InterfaceEX/InterfaceEX/ISoundProducable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceEX
+{
+    public interface ISoundProducable
+    {
+        string ProduceSound();
+    }
+}
--- InterfaceEX/InterfaceEX/Program.cs
+++ InterfaceEX/InterfaceEX/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InterfaceEX
 {
@@ -6,7 +7,40 @@ namespace InterfaceEX
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Animal> animals = new List<Animal>();
+            string name,gender;
+            int age;
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Beast!")
+            {
+                Console.WriteLine("Enter name:");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter age:");
+                age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter gender:");
+                gender = Console.ReadLine();
+                try
+                {
+                    switch (command)
+                    {
+                        case "Cat":
+                            animals.Add(new Cat(name, age, gender)); break;
+                        case "Dog":
+                            animals.Add(new Dog(name, age, gender)); break;
+                      
+                        default:
+                            throw new ArgumentException("Invalid input!");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            animals.ForEach(Console.WriteLine);
         }
     }
 }
